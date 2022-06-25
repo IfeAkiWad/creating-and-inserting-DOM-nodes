190147b150979a583edf5978127510de63c82747
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-06-24 23:49:24 -0500

    add AVI

--- js/index.js
+++ js/index.js
@@ -9,3 +9,19 @@ pageTitle.innerHTML = 'My Page'
 document.body.appendChild(pageTitle)
 pageTitle.style.color = "white"
 pageTitle.style.textAlign = "center"
+pageTitle.style.fontSize = "larger"
+
+// image
+let name = document.createElement('div')
+let imgDiv = document.createElement('div')
+document.body.appendChild(imgDiv)
+imgDiv.setAttribute("style", "margin:0auto;display:flex;align-items:center;")
+
+let image = document.createElement('img')
+image.setAttribute("style", "width:200px;height:200px;")
+image.setAttribute("alt", "smiley face")
+image.src = "https://c8.alamy.com/comp/F4JBW6/a-cool-looking-emoji-emoticon-smiley-face-character-with-sunglasses-F4JBW6.jpg"
+imgDiv.appendChild(image)
+
+
+
