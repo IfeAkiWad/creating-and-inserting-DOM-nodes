479bb075764daa24ecece6e8ba057b509397eb1e
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-06-25 00:14:46 -0500

    create about  me

--- js/index.js
+++ js/index.js
@@ -12,7 +12,6 @@ pageTitle.style.textAlign = "center"
 pageTitle.style.fontSize = "larger"
 
 // image
-let name = document.createElement('div')
 let imgDiv = document.createElement('div')
 document.body.appendChild(imgDiv)
 imgDiv.setAttribute("style", "margin:0auto;display:flex;align-items:center;")
@@ -22,6 +21,30 @@ image.setAttribute("style", "width:200px;height:200px;")
 image.setAttribute("alt", "smiley face")
 image.src = "https://c8.alamy.com/comp/F4JBW6/a-cool-looking-emoji-emoticon-smiley-face-character-with-sunglasses-F4JBW6.jpg"
 imgDiv.appendChild(image)
+
+//about me
+let aboutMe = document.createElement('div')
+document.body.appendChild(aboutMe)
+aboutMe.setAttribute('class', 'about-me')
+aboutMe.innerHTML = "About Me"
+aboutMe.style.color = "white"
+aboutMe.style.textAlign = "center"
+aboutMe.style.fontSize = "larger"
+
+// About me form
+let aboutForm = document.createElement('div') 
+document.body.appendChild(aboutForm)
+aboutForm.innerHTML += 
+`
+<form id='form'> 
+    <textarea id="description" required></textarea><br><br>
+    <input type="submit" value="Submit">
+    <br><br>
+</form>
+`
+
+aboutForm.style.textAlign = "center"
+
 
 
 
